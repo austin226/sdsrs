0f47ef9381fb8812695c5c8cf1991938bfb8b264
Austin Almond <austin226@live.com>
2017-05-03 22:12:52 -0400

    Fix index.php/hello/world

--- src/public/index.php
+++ src/public/index.php
@@ -4,8 +4,6 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
-echo "HI";
-exit();
 
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response) {
