b63477f1dfdaca921dcba0d626fb8b1b7b452acc
Austin Almond <austin226@live.com>
2017-05-17 08:50:15 -0400

    Create api controller with namespace

--- api.php
+++ api.php
@@ -2,3 +2,9 @@
 
 require 'vendor/autoload.php';
 
+use Austin226\Sdsrs\ApiController;
+
+$ankiServerUri = 'http://localhost:3000';
+$apiController = new ApiController($ankiServerUri);
+
+var_dump($apiController);
--- src/ApiController.php
+++ src/ApiController.php
@@ -1,8 +1,20 @@
 <?php
 
+namespace Austin226\Sdsrs;
+
+use GuzzleHttp\Client;
+
 class ApiController
 {
+    public function __construct($ankiServerUri)
+    {
+        $this->ankiServerClient = new Client([
+            'base_uri' => $ankiServerUri
+        ]);
+    }
+
     public function listCollections()
     {
+
     }
 }
