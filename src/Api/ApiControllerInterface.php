815086c99de37d23709dae1ece35057d2d7bd64e
Austin Almond <austin226@live.com>
2017-05-26 08:29:21 -0400

    Add method for api interface

--- src/Api/ApiControllerInterface.php
+++ src/Api/ApiControllerInterface.php
@@ -4,4 +4,16 @@ namespace Aalmond\Sdsrs\Api;
 
 interface ApiControllerInterface
 {
+    /**
+     * Performs an action, and returns the result to be
+     * sent to the client.
+     *
+     * @param string $method HTTP method used
+     * @param string $action action to perform
+     * @param array $requestData
+     *
+     * @return array
+     * @throws \Aalmond\Sdsrs\Exceptions\HttpException
+     */
+    public function doAction(string $method, string $action, array $requestData) : array;
 }
