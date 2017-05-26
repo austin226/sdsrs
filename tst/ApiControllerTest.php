2a3c1adacdd814eccfe63e34dc373f81950825df
Austin Almond <austin226@live.com>
2017-05-25 20:10:09 -0400

    Test bad request

--- tst/ApiControllerTest.php
+++ tst/ApiControllerTest.php
@@ -3,6 +3,7 @@
 namespace Austin226\Sdsrs\tst;
 
 use Austin226\Sdsrs\ApiController;
+use Austin226\Sdsrs\Exceptions\BadRequestException;
 use Austin226\Sdsrs\Exceptions\MethodNotAllowedException;
 use PHPUnit\Framework\TestCase;
 
@@ -19,5 +20,11 @@ class ApiControllerTest extends TestCase
     {
         $this->expectException(MethodNotAllowedException::class);
         $this->apiController->doAction('POST', 'list_collections', []);
+    }
+
+    public function testBadAction()
+    {
+        $this->expectException(BadRequestException::class);
+        $this->apiController->doAction('POST', 'blah', []);
     }
 }
