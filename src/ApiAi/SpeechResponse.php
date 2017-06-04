6dc829affdddb375271abb273c690e9d6d45303e
Austin Almond <austin226@live.com>
2017-06-03 21:07:13 -0400

    Serialize SpeechResponse

--- src/ApiAi/SpeechResponse.php
+++ src/ApiAi/SpeechResponse.php
@@ -15,14 +15,36 @@ class SpeechResponse implements \JsonSerializable
         string $speech,
         string $displayText,
         array $data,
-        array $contextOut = [],
-        string $source = '',
-        array $followupEvent = []
+        ?array $contextOut = null,
+        ?string $source = null,
+        ?array $followupEvent = null
     ) {
-        $this->speech = $speech;    
+        $this->speech = $speech;
+        $this->displayText = $displayText;
+        $this->data = $data;
+        $this->contextOut = $contextOut;
+        $this->source = $source;
+        $this->followupEvent = $followupEvent;
     }
 
     public function jsonSerialize()
     {
+        $jsonData = [
+            'speech' => $this->speech,
+            'displayText' => $this->displayText,
+            'data' => $this->data
+        ];
+
+        if (isset($this->contextOut)) {
+            $jsonData['contextOut'] = $this->contextOut;
+        }
+        if (isset($this->source)) {
+            $jsonData['source'] = $this->source;
+        }
+        if (isset($this->followupEvent)) {
+            $jsonData['followupEvent'] = $this->followupEvent;
+        }
+
+        return $jsonData;
     }
 }
